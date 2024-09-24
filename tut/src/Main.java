68138c968c072ac9e6ddee1f484b4f9ee803840a
Aimable01 <aimable.kwizera14@gmail.com>
2024-09-24 10:28:32 +0200

    feat: encapsulation and inheritence

--- tut/src/Main.java
+++ tut/src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+
+        Inventory inventory = new Inventory();
+
+        Item item = new Item("Generic item",10);
+        Fruit fruit = new Fruit("Apple",20,"Fuji");
+
+        inventory.addItem(item);
+        inventory.addItem(fruit);
+
+        inventory.displayInventory();
     }
 }
