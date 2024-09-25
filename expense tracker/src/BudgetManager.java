016b0a9e81dac16a2d4588bcb55faedc17284c96
Aimable01 <aimable.kwizera14@gmail.com>
2024-09-25 21:02:25 +0200

    feat: add budget manager logic

--- expense tracker/src/BudgetManager.java
+++ expense tracker/src/BudgetManager.java
@@ -1,7 +1,71 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class BudgetManager {
-    private ArrayList<Transaction> transactions;
+    private List<Transaction> transactions;
+    private final String FILE_PATH = "Output_Data.txt";
 
+    public BudgetManager() {
+        transactions = new ArrayList<>();
+    }
 
+    // add transaction
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    // list transactions
+    public void listTransactions() {
+        if(transactions.isEmpty()) {
+            System.out.println("There are no transactions");
+        }else {
+            for (Transaction transaction : transactions) {
+                System.out.println(transaction);
+            }
+        }
+    }
+
+    // get total income
+    public double getTotalIncome(){
+        double total = 0;
+        for (Transaction transaction : transactions) {
+            if(transaction.getType().equalsIgnoreCase("income")){
+                total += transaction.getAmount();
+            }
+        }
+        return total;
+    }
+
+    // get total expenses
+    public double getTotalExpense(){
+        double total = 0;
+        for (Transaction transaction : transactions) {
+            if(transaction.getType().equalsIgnoreCase("expense")){
+                total += transaction.getAmount();
+            }
+        }
+        return total;
+    }
+
+    // get balance
+    public double getBalance(){
+        return getTotalIncome() + getTotalExpense();
+    }
+
+    // save transactions
+    public void saveTransactions(){
+        try {
+            FileWriter fileWriter = new FileWriter(FILE_PATH);
+            for (Transaction transaction : transactions) {
+                fileWriter.write(transaction.toFileFormat() + "\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error while saving transactions" + e.getMessage());
+        }
+    }
+
+    // load transactions
 }
